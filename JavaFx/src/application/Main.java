0f9c26258b1e03fdb83f9df9fe14f27e7684271e
0rakul0 <0rakul0render@gmail.com>
2020-10-30 01:29:59 -0300

    att de pastas

---
+++ JavaFx/src/application/Main.java
@@ -0,0 +1,29 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			 Parent parent = loader.load();
+			 Scene mainScene = new Scene(parent);
+			 primaryStage.setScene(mainScene);
+			 primaryStage.setTitle("Sample JavaFX application");
+			 primaryStage.show(); 
+			 mainScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
