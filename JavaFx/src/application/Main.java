6bd5a6db0f1c3c8fcbb0e1bf808e6de16a83799f
0rakul0 <0rakul0render@gmail.com>
2020-10-30 03:45:15 -0300

    criada a tela editar

--- JavaFx/src/application/Main.java
+++ JavaFx/src/application/Main.java
@@ -1,31 +1,37 @@
 package application;
-	
+
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			 ScrollPane scrollpane = loader.load();
+			ScrollPane scrollPane = loader.load();
 			
-			 // faz com que o menu fique por toda a extremidade da tela
-			 scrollpane.setFitToHeight(true);
-			 scrollpane.setFitToWidth(true);
-			 
-			 Scene mainScene = new Scene(scrollpane);
-			 primaryStage.setScene(mainScene);
-			 primaryStage.setTitle("Sample JavaFX application");
-			 primaryStage.show(); 
-			 mainScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		} catch(Exception e) {
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			
+			mainScene = new Scene(scrollPane);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Sample JavaFX application");
+			primaryStage.show();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 	
 	public static void main(String[] args) {
--- JavaFx/src/gui/MainViewController.java
+++ JavaFx/src/gui/MainViewController.java
@@ -1,57 +1,67 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
-		//menuOperacional
-		@FXML
-		private MenuItem menuItemAluno;
-		//menuEditar
-		@FXML
-		private MenuItem menuItemVer;
-		@FXML
-		private MenuItem menuItemAtualizar;
-		@FXML
-		private MenuItem menuItemDeletar;
-		@FXML
-		private MenuItem menuItemCriar;
-		
-		//declaração da função
-		@FXML
-		public void onMenuItemAlunoAction() {
-			System.out.println("clicou em aluno");
-		}
-		
-		@FXML
-		public void onMenuItemVerAction() {
-			System.out.println("clicou em ver");
-		}
-		
-		@FXML
-		public void onMenuItemAtualizarAction() {
-			System.out.println("clicou em att");
-		}
-		
-		@FXML
-		public void onMenuItemDeletarAction() {
-			System.out.println("clicou em deletar");
-		}
-		
-		@FXML
-		public void onMenuItemCriarAction() {
-			System.out.println("clicou em vriar");
-		}
-		
-		
-		@Override
-		public void initialize(URL uri, ResourceBundle rb) {
-			// TODO Auto-generated method stub
+	// menuOperacional
+	@FXML private MenuItem menuItemAluno;
+	// menuEditar
+	@FXML private MenuItem menuItemVer;
+	@FXML private MenuItem menuItemAtualizar;
+	@FXML private MenuItem menuItemDeletar;
+	@FXML private MenuItem menuItemCriar;
+
+	// declaração da função
+	@FXML public void onMenuItemAlunoAction() {
+		System.out.println("clicou em aluno");
+	}
+	@FXML public void onMenuItemVerAction() {
+		loadView("/gui/Editar.fxml");
+	}
+	@FXML public void onMenuItemAtualizarAction() {
+		System.out.println("clicou em att");
+	}
+	@FXML public void onMenuItemDeletarAction() {
+		System.out.println("clicou em deletar");
+	}
+	@FXML public void onMenuItemCriarAction() {
+		System.out.println("clicou em vriar");
+	}
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {
+		// TODO Auto-generated method stub
+	}
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
 			
-}
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
 
 }
